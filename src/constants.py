1e8107805e69f9bc9cd6ee8eab0d87d0d260903d
ludakriess <hendsaid2004@gmail.com>
2026-02-08 00:37:49 +0100

    added constants

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PARQUET_PATH = os.path.join(BASE_DIR, "data", "consumed", "cleaned_consumed.parquet")
+KAFKA_BROKER = "localhost:9092"
+KAFKA_TOPIC = "redset-group"
+CLEANED_PATH = os.path.join(BASE_DIR, "data", "consumed", "cleaned_consumed.parquet")
+METRICS_DIR = os.path.join(BASE_DIR, "src", "metrics")
+DB_PATH = os.path.join(BASE_DIR, "artifacts", "metrics.duckdb")
+COST = 0.4278
+NO_OF_RPUS = 128
